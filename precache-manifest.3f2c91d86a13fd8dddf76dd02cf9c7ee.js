09cb0d38d9b7485b801bfc64d15c12716b737012
Navid Dezashibi <dezashibi@gmail.com>
2019-12-22 11:53:59 +0330

    Updates

---
+++ precache-manifest.3f2c91d86a13fd8dddf76dd02cf9c7ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aedf039b67bb249198f7aed57f9dfd3",
+    "url": "/react-fun-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "299bda84a82d8fdb00bb",
+    "url": "/react-fun-monsters-rolodex/static/css/main.5415cb49.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/react-fun-monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-fun-monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "299bda84a82d8fdb00bb",
+    "url": "/react-fun-monsters-rolodex/static/js/main.2e7ab8d7.chunk.js"
+  },
+  {
+    "revision": "07be9b8d4eee6fc6760c",
+    "url": "/react-fun-monsters-rolodex/static/js/runtime-main.cf8e63d8.js"
+  }
+]);
